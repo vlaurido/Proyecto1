a8b8b73beb3a8e666bf379efe69a76002192e967
Alexis <alexisleo15@gmail.com>
2017-06-25 17:23:30 -0500

    commit

--- src/Lista_Sacar.c
+++ src/Lista_Sacar.c
@@ -7,8 +7,8 @@ void Lista_Sacar(ListaEnlazada *lista, ElementoLista *elemento) {
 	if ((lista != NULL) && (elemento != NULL) && !Lista_Vacia(lista)) {
 		ElementoLista *aux = Lista_Buscar(lista,elemento->objeto);
 		if (aux != NULL) {
-			ElementoLista *siguiente = Lista_Siguiente(lista,elemento);
-			ElementoLista *anterior = Lista_Anterior(lista,elemento);
+			ElementoLista *siguiente = elemento->siguiente;
+			ElementoLista *anterior = elemento->anterior;
 			if (elemento == Lista_Ultimo(lista)) {
 				anterior->siguiente = &(lista->ancla);
 				lista->ancla.anterior = anterior;
--- src/Lista_Siguiente.c
+++ src/Lista_Siguiente.c
@@ -5,20 +5,8 @@ ElementoLista *Lista_Siguiente(ListaEnlazada *lista, ElementoLista *elemento);
 
 ElementoLista *Lista_Siguiente(ListaEnlazada *lista, ElementoLista *elemento){
 	if ((lista!=NULL) && (elemento!=NULL) && !Lista_Vacia(lista)){
-		/*ElementoLista *aux = &(lista->ancla);
-		if(elemento->siguiente==aux){
-			return NULL;
-		}else{
-			while((aux->siguiente)!=&(lista->ancla)){
-  				if (aux == elemento){
-    				ElementoLista *elem = (ElementoLista *)malloc(sizeof(ElementoLista));
-					elem = aux->siguiente;
-					return elem;
-				}
-  				aux = aux->siguiente;
-			}return NULL;
-		}*/
-		ElementoLista *aux = Lista_Ultimo(lista);
+		ElementoLista *aux = (ElementoLista *)malloc(sizeof(ElementoLista));
+		aux = Lista_Ultimo(lista);
 		if (elemento != aux)
 			return elemento->siguiente;
 	}
